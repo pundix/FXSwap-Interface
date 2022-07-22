bca206c3aefda2f8f6f34291f56db192b76a1a43
Yan Ping <yanping@Yans-MacBook-Air-2.local>
2022-07-22 18:12:36 +0800

    Added support for FXMainnet

--- src/@fxswap/v2-sdk/dist/constants.d.ts
+++
@@ -1,9 +0,0 @@
-import JSBI from 'jsbi';
-export declare const FACTORY_ADDRESS = "0x9E229BE3812228454499FAf771b296bedFe8c904";
-export declare const INIT_CODE_HASH = "0x89bbd161b7f9309df13e388d6b1f71f5ff07259b5149560e726a4df10971bceb";
-export declare const MINIMUM_LIQUIDITY: JSBI;
-export declare const ZERO: JSBI;
-export declare const ONE: JSBI;
-export declare const FIVE: JSBI;
-export declare const _997: JSBI;
-export declare const _1000: JSBI;
